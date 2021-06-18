bbb62bfd16acb69914ce116cf9d6d4dc2dab5652
BrunoMatosSilva <brunomatos2010@outlook.com>
2021-06-18 16:51:05 -0300

    miragejs

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,11 +1,15 @@
-import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpointValue } from '@chakra-ui/react'
+import { Box, Button, Checkbox, Flex, Heading, Icon, Spinner, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpointValue } from '@chakra-ui/react'
 import Link from 'next/link';
 import { RiAddLine } from 'react-icons/ri';
 import { Header } from '../../components/Header'
 import { Pagination } from '../../components/Pagination';
 import { Sidebar } from '../../components/Sidebar'
+import { useUsers } from '../../services/hooks/useUsers';
 
 export default function UserList() {
+
+    const { data, isLoading, isFetching, error } = useUsers()
+
     const isWideVersion = useBreakpointValue({
         base: false,
         lg: true,
@@ -20,7 +24,10 @@ export default function UserList() {
 
                 <Box flex="1" borderRadius={8} bg="gray.800" p="8">
                     <Flex mb="8" justify="space-between" align="center">
-                        <Heading size="lg" fontWeight="normal">Usuários</Heading>
+                        <Heading size="lg" fontWeight="normal">
+                            Usuários
+                            {!isLoading && isFetching && <Spinner size="sm" color="gray.500" ml="4" />}
+                        </Heading>
 
                         <Link href="/users/create" passHref>
                             <Button
@@ -32,71 +39,51 @@ export default function UserList() {
                                     fontSize="20" />
                                 }>
                                 Criar novo
-                        </Button>
+                            </Button>
                         </Link>
                     </Flex>
 
-                    <Table colorScheme="whiteAlpha">
-                        <Thead>
-                            <Tr>
-                                <Th px={["4", "4", "6"]} color="gray.300" width="8">
-                                    <Checkbox colorScheme="pink" />
-                                </Th>
-                                <Th>Usuário</Th>
-                                {isWideVersion && <Th>Data de cadastro</Th>}
-                            </Tr>
-                        </Thead>
-                        <Tbody>
-                            <Tr>
-                                <Td px={["4", "4", "6"]}>
-                                    <Checkbox colorScheme="pink" />
-                                </Td>
-                                <Td>
-                                    <Box>
-                                        <Text fontWeight="bold">Diego Fernandes</Text>
-                                        <Text fontSize="small" color="gray.300">diego.schell.f@gmail.com</Text>
-                                    </Box>
-                                </Td>
-                                {isWideVersion && <Td>
-                                    04 de Abril, 2021
-                                </Td>}
+                    {isLoading ? (
+                        <Flex justify="center">
+                            <Spinner />
+                        </Flex>
+                    ) : error ? (
+                        <Flex justify="center">Falha ao obter dados dos usuários.</Flex>
+                    ) : (
+                        <>
+                            <Table colorScheme="whiteAlpha">
+                                <Thead>
+                                    <Tr>
+                                        <Th px={["4", "4", "6"]} color="gray.300" width="8">
+                                            <Checkbox colorScheme="pink" />
+                                        </Th>
+                                        <Th>Usuário</Th>
+                                        {isWideVersion && <Th>Data de cadastro</Th>}
+                                    </Tr>
+                                </Thead>
+                                <Tbody>
+                                    {data.map(user => {
+                                        return (
+                                            <Tr key={user.id}>
+                                                <Td px={["4", "4", "6"]}>
+                                                    <Checkbox colorScheme="pink" />
+                                                </Td>
+                                                <Td>
+                                                    <Box>
+                                                        <Text fontWeight="bold">{user.name}</Text>
+                                                        <Text fontSize="small" color="gray.300">{user.email}</Text>
+                                                    </Box>
+                                                </Td>
+                                                {isWideVersion && <Td>{user.createdAt}</Td>}
 
-                            </Tr>
-
-                            <Tr>
-                                <Td px={["4", "4", "6"]}>
-                                    <Checkbox colorScheme="pink" />
-                                </Td>
-                                <Td>
-                                    <Box>
-                                        <Text fontWeight="bold">Diego Fernandes</Text>
-                                        <Text fontSize="small" color="gray.300">diego.schell.f@gmail.com</Text>
-                                    </Box>
-                                </Td>
-                                {isWideVersion && <Td>
-                                    04 de Abril, 2021
-                                </Td>}
-
-                            </Tr>
-
-                            <Tr>
-                                <Td px={["4", "4", "6"]}>
-                                    <Checkbox colorScheme="pink" />
-                                </Td>
-                                <Td>
-                                    <Box>
-                                        <Text fontWeight="bold">Diego Fernandes</Text>
-                                        <Text fontSize="small" color="gray.300">diego.schell.f@gmail.com</Text>
-                                    </Box>
-                                </Td>
-                                {isWideVersion && <Td>
-                                    04 de Abril, 2021
-                                </Td>}
-
-                            </Tr>
-                        </Tbody>
-                    </Table>
-                    <Pagination />
+                                            </Tr>
+                                        )
+                                    })}
+                                </Tbody>
+                            </Table>
+                            <Pagination />
+                        </>
+                    )}
                 </Box>
             </Flex>
         </Box>
